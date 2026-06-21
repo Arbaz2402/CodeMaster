8bbd49429c0563ca64218231fbcb8dbf652e4c95
Arbaz2402 <kaladiya.arbaz@gmail.com>
2026-06-21 18:00:20 +0530

    Update API base URL to use Render backend

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://codemaster-backend-20m.onrender.com/api';
 
 // Helper to get stored token
 const getToken = () => localStorage.getItem('codemaster_token');
